bdc8adc789154dad265d188812a0b3a7e82b4333
Phillip Friedel <s0557917@htw-berlin.de>
2023-02-22 10:38:30 +0100

    Moved weather display switch to own helper file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { useEffect, useState } from "react"
 import WeatherCard from "./components/WeatherCard"
-import SunnyIcon from "./components/icons/ClearIcon"
-import CloudyIcon from "./components/icons/CloudyIcon";
-import StormIcon from "./components/icons/StormIcon";
-import RainCloud from "./components/icons/RainIcon";
-import SnowIcon from "./components/icons/SnowIcon"
 import checkIfObjectIsEmpty from "./utils/helpers"
 import { getDailyWeather } from "./utils/date-time";
 import countryCityList from "./data/country-city-list"; 
 import CityInput from "./components/CityInput";
+import { weatherDisplaySwitch } from "./utils/weather-display-switch";
 
 import { fetchJSONData, fetchSVGData } from "./utils/fetch";
 
@@ -27,27 +23,6 @@ function App() {
   const [flag, setFlag] = useState(<h1>Flag</h1>)
   
   const debouncedValue = useDebounce(cityInputValue, 500)
-
-  const switchIcon = () => {
-      const weatherId = weather.weather[0].id
-
-      if(weatherId === 800) {
-        setBackground("bg-clear")
-        setWeatherLogo(<SunnyIcon />)
-      } else if(weatherId > 800 && weatherId < 900) {
-        setBackground("bg-cloudy")
-        setWeatherLogo(<CloudyIcon />)
-      } else if(weatherId > 200 && weatherId < 300) {
-        setBackground("bg-stormy")
-        setWeatherLogo(<StormIcon />)
-      } else if(weatherId > 300  && weatherId < 400 || weatherId > 500  && weatherId < 600) {
-        setBackground("bg-rain")
-        setWeatherLogo(<RainCloud />)
-      } else if(weatherId > 600 && weatherId < 700) {
-        setBackground("bg-rain")
-        setWeatherLogo(<SnowIcon />)
-      }
-  }
 
   async function fetchCityName(lat, lon) {
     const url = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${import.meta.env.VITE_WEATHER_API}`
@@ -140,30 +115,16 @@ function App() {
       getWeather()
     }
   }, [debouncedValue])
-  
-
-  // useEffect(() => {
-  //   async function fetchCountries() {
-  //     const url = `http://battuta.medunes.net/api/country/all/?key=${import.meta.env.VITE_CITY_API}`
-  //     const options = {
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //         'Access-Control-Allow-Origin': '*'
-  //       }
-  //     }
-  //     const response = await fetch(url, options)
-  //     const data = await response.json()
-  //     console.log("COUNTRIES", data)
-  //     setCountries(data)
-  //   }
-
-  //   fetchCountries()
-  // }, [])
 
 
   useEffect(() => {
     if(!checkIfObjectIsEmpty(weather)) {
-        switchIcon()
+        const {bg, icon} = weatherDisplaySwitch(weather.weather[0].id)
+        if(bg !== "" && icon !== <></>) {
+          setBackground(bg)
+          setWeatherLogo(icon)
+        }
+
     }
   },[weather])
   
---
+++ src/utils/weather-display-switch.jsx
@@ -0,0 +1,30 @@
+import SunnyIcon from "../components/icons/ClearIcon"
+import CloudyIcon from "../components/icons/CloudyIcon";
+import StormIcon from "../components/icons/StormIcon";
+import RainCloud from "../components/icons/RainIcon";
+import SnowIcon from "../components/icons/SnowIcon"
+
+export const weatherDisplaySwitch = (weatherId) => {
+    
+    let bg = ""
+    let icon = <></>
+    
+    if(weatherId === 800) {
+        bg = "bg-clear"
+        icon = <SunnyIcon />
+    } else if(weatherId > 800 && weatherId < 900) {
+        bg = "bg-cloudy"
+        icon = <CloudyIcon />
+    } else if(weatherId > 200 && weatherId < 300) {
+        bg = "bg-stormy"
+        icon = <StormIcon />
+    } else if(weatherId > 300  && weatherId < 400 || weatherId > 500  && weatherId < 600) {
+        bg = "bg-rain"
+        icon = <RainCloud />
+    } else if(weatherId > 600 && weatherId < 700) {
+        bg = "bg-rain"
+        icon = <SnowIcon />
+    }
+
+    return {bg, icon}
+}
